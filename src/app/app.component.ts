009d38c985e08ed92326ca4819e6af43cc7f7c54
Andres <andresgueno@hotmail>
2023-07-06 17:58:33 -0500

    no se

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { throttle } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Calculadora';
+  input = '';
+  binding = '0';
+
+  agregarNumero(number: string){
+  this.input = this.input + number;
+  this.binding = this.input;
+  }
+
+  igual(){
+    this.binding = eval(this.binding);
+  }
+
+  clear(){
+    this.binding = '0';
+    this.input = '';
+  }
+
+  clearAc(){
+    this.binding = this.binding.slice(0, -1);
+  }
+
 }
